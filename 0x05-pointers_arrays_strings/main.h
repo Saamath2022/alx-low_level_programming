d0dd11d9ba3809e0914a68a0716b54fb64d7cfc0
Saamath2022 <saaaondow@gmail.com>
2022-12-19 19:00:47 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+void reset_to_98(int *n);
+	void swap_int(int *a, int *b);
+	int _strlen(char *str);
+	int _putchar(char c);
+	void print_rev(char *s)
+	void put2(char *str);
+	void puts_arrary(int *a, int n);
+	char *_strcpy(char *dest, char *strc);
+	void rev_string(char *s);
+
