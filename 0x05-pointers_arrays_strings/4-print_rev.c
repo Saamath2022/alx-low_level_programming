d7b931672fc8ee22c79fcab5efa6fb2201501a24
Saamath2022 <saaaondow@gmail.com>
2022-12-22 06:26:26 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints a string in reverse
+ * @rev: Reverse
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i -1; i >= 0; i--)
+		_putchar(s [i]);
+
+	_putchar('\n');
+}
