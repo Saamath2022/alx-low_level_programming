b02b49b71c854ae4a54f5d09b23b79e5e6baa9b7
Saamath2022 <saaaondow@gmail.com>
2022-12-22 02:29:30 -0800

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,5 @@
 #include "main.h"
-<<<<<<< HEAD
+#include <stdio.h>
 
 /**
  * swap_int - swaps the values of two integers
@@ -8,24 +8,9 @@
  */
 void swap_int(int *a, int *b)
 {
-	int m;
-
-	m = *a;
-	*a = *b;
-	*b = m;
-=======
-/**
- * main - check the code
- * @a: int a
- * @b: int b
- */
-void swap_int(int *a, int *b)
-{
 	int q;
-	
 
 	q = *a;
 	*a = *b;
 	*b = q;
->>>>>>> fb6ff520694f4b4f72161d4fa2c37ffe951bdb90
 }
