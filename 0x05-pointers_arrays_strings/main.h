2fabbba8c2dc06b0bbb4c3e7608268e9b41d6b39
Saamath2022 <saaaondow@gmail.com>
2022-12-21 16:46:26 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,33 +1,22 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strilen(char *s);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *strc);
+char *_strcpy(char *dest, char *src);
 
 #endif
-=======
-void reset_to_98(int *n);
-	void swap_int(int *a, int *b);
-	int _strlen(char *s);
-	void _puts(char *str);
-	int _putchar(char c);
-	void print_rev(char *s)
-	void put2(char *str);
-	void puts_arrary(int *a, int n);
-	char *_strcpy(char *dest, char *strc);
-	void rev_string(char *s);
-
->>>>>>> e2ab5216734ac096db4d6dabc94e161ba8a5cc2b
