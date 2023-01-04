7736bc4a85e66155a207d68dc23480c65e326c20
Saamath2022 <saaaondow@gmail.com>
2023-01-04 09:12:52 -0800

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,22 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * file: main.h
- * Auth: Masa tech (Saa-Aondo Mathias)
- * description: header file containing prototypes for all function
- * used in the 0x0-pointers_arrays_strings director
- */
-
-#include <stddef.h>
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(chr *s, char *accept);
-char *_strchr(char *s, haystack, char *needle);
-void print_chessboard(char (*a)[8]);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);	
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
