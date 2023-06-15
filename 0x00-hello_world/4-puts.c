6a35cadc835007841843cd46acda027ab1608072
Saamath2022 <saaaondow@gmail.com>
2023-06-15 15:47:30 -0700

    task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - a program that print a write up using puts funtion
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+
+	return (0);
+}
