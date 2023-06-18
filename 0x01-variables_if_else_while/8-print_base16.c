b8566f02fef59f37e9d75e37fecb1804e495f801
Saamath2022 <saaaondow@gmail.com>
2023-06-18 07:11:15 -0700

    task 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,8 +12,9 @@ int main(void)
 {
 	int num;
 	char la;
-	for (num =0; num < 10; num++)
-		putchar((num %10) +'0');
+
+	for (num = 0; num < 10; num++)
+		putchar ((num % 10) + '0');
 
 	for(la = 'a'; la <= 'f'; la++)
 		putchar(la);
