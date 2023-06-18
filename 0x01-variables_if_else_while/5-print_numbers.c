b17f1901fbfb89e142a1f9746238bdaf15f7bdd1
Saamath2022 <saaaondow@gmail.com>
2023-06-18 04:45:28 -0700

    task 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,14 @@
  * Return: 0 (Success aways)
  */
 
-int main(void) 
+int main(void)
 {
 	int number;
 
 	for (number = 0; number <= 9; number++)
-	
-	putchar(number + '0');
-
+	{
+		putchar(number + '0');
+	}
 	putchar('\n');
 	return 0;
 }
