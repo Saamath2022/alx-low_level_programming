f7115f950efe7b28f0ecfab13eb35e441f18513a
Saamath2022 <saaaondow@gmail.com>
2023-06-18 07:32:19 -0700

    task 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - write a function that prints all possible cominations of 
+ * single-digit numbers.
+ * Return: Aways 0 (Success)
+ */
+
+int main(void)
+{
+    int x;
+
+    for (x = 0; x < 10; x++)
+    {
+	    putchar (x + '0');
+	    if (x < 9)
+	    {
+            	putchar (',');
+            	putchar (' ');
+	    }
+    }
+	putchar ('\n');
+	return (0);
+}
