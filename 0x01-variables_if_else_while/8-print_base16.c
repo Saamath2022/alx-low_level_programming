d64b53577b953faa56f53defe772ebc22772ecd8
Saamath2022 <saaaondow@gmail.com>
2023-06-18 07:03:32 -0700

    task 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - programe that prints all the nubmers of base 16 in lowercase,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char la;
+	for (num =0; num < 10; num++)
+		putchar((num %10) +'0');
+
+	for(la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
