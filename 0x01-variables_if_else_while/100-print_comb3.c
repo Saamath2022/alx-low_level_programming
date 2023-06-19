4536031dec04e6f761740179ecf741daa6ecd543
Saamath2022 <saaaondow@gmail.com>
2023-06-18 21:01:32 -0700

    task 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,9 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num2 < 9; num2++)
-	{
+	for (num1 = 0; num1 < 9; num1++){
+		for(num2 = num1+1; num2 < 10; num2++){
+		
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
 
@@ -22,8 +23,8 @@ int main(void)
 		continue;
 		putchar (',');
 		putchar (' ');
-	}
+		}
 	putchar ('\n');
-
+	}
 	return (0);
 }
