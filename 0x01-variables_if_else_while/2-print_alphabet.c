bdf40986d1cca1ac7fb92fbd1f038413183b8e4c
Saamath2022 <saaaondow@gmail.com>
2023-06-18 19:08:31 -0700

    task 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,12 @@
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
 
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
