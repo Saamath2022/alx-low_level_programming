ca6175d2c48fb919fc35894bc922f516d9876da6
Saamath2022 <saaaondow@gmail.com>
2023-06-18 19:21:45 -0700

    task 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,19 +3,24 @@
 #include <time.h>
 
 /**
- * main - a program that prints the alphabet in lowercase, followed by a new line.
+ * main - a program that prints the alphabet in lowercase,
+ * followed by a new line.
  *
  * Return: Always 0 (Succes)
  */
 
-int main() {
+int main(void)
+{
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++) {
-		if (letter != 'q' && letter != 'e') {
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
 			putchar(letter);
-        }
-    }
-	putchar('\n');
-	return 0;
+        	}
+	}
+	putchar ('\n');
+
+	return (0);
 }
