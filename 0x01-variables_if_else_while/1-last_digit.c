3a8a2a96ed9c27129c968eae3b23a692c0d303cb
Saamath2022 <saaaondow@gmail.com>
2023-06-18 18:44:28 -0700

    task 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,9 @@
 #include <time.h>
 
 /**
- * main last digit of n is greater than 5: the string and is greater than 5
- * Return 0 (Success)
+ * main - function to print the last digit of n is greater than 5:
+ * the string and is greater than 5
+ * Return: 0 (Success)
  */
 
 int main(void)
@@ -14,20 +15,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	/* your code goes there */
+
 	a = n % 10;
-	if (a > 5)
+	if(a > 5)
 	{
-		printf ("Last of %d is %d and is greater than 5\n", n, a);
+		printf("Last of %d is %d and is greater than 5\n", n, a);
 	}
-	else if (a == 0)
+	else if(a == 0)
 	{
-		printf ("Last digit of %d is %d and is 0\n", n, a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 	else
 	{
-		printf ("Last digit of %d is %d and less than 6 is not 0\n", n, a);
+		printf("Last digit of %d is %d and less than 6 is not 0\n", n, a);
 	}
 
 	return (0);
