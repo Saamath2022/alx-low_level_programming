436f6ecf4f4887e9d3c807a51b5e8d386f6f0285
Saamath2022 <saaaondow@gmail.com>
2023-06-21 07:15:17 -0700

    task 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Checks_lower - Write a function that 
+ * checks for lowercase
+ * returns: 1
+ */
+
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'a')
+	{
+	return 1;
+	}
+	else
+	{
+	return 0;
+	}
+}
