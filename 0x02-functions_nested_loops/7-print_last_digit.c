105049299aa80f0056fc3e72e87279fc983b49e3
Saamath2022 <saaaondow@gmail.com>
2023-06-21 11:47:27 -0700

    task 7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - print the last digit of a number
+ * Return: value of the last digit
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
