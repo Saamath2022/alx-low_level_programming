75128478fe7c5e26dd9516f972f4ab837f430e05
Saamath2022 <saaaondow@gmail.com>
2023-06-21 11:09:04 -0700

    task 5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,13 @@
  * 0 of number is zero
  * -1 if number is less than zero.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		return 1;
 	}
 	else if (n == 0)
 	{
@@ -22,7 +23,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
 	}
 }
