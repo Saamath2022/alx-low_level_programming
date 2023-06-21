a368e58878decd3a0e0e670ebb1e763613ab7752
Saamath2022 <saaaondow@gmail.com>
2023-06-21 06:17:41 -0700

    task 2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main -  function that prints 10 times the alphabet, in lowercase
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9) 
+	{
+	for (c = 'a'; c <= 'z'; c++) 
+	{
+	_putchar(c);
+	}
+	_putchar ('\n');
+	i++;
+	}
+}
