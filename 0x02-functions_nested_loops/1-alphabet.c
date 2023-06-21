dab7674594044a7c822a6457a76ba2bb868a4504
Saamath2022 <saaaondow@gmail.com>
2023-06-21 04:19:47 -0700

    task 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - in lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+char ch; 
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
