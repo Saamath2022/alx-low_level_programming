44765691748cd6e5634c30ec2f343a487fe05fb0
Saamath2022 <saaaondow@gmail.com>
2023-06-23 17:43:41 -0700

    task 1.isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for the last digit
+ * @c: input character
+ * Return: (1) if it is true and (0) if it is false
+ */
+
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
