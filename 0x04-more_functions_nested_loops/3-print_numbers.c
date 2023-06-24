1b5ccec489ee8d0282d9eaef64d9a20f1514933d
Saamath2022 <saaaondow@gmail.com>
2023-06-24 01:17:07 -0700

    task 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checs for a digit( 0 through 9)
+ * print_numbers -  print numbers from 0 to 9
  *
  * Return: Always 0.
  */
@@ -9,6 +9,7 @@
 void print_numbers(void)
 {
 	int c;
+
 	for (c = 0; c <= 9; c++)
 	{
 	_putchar(c);
