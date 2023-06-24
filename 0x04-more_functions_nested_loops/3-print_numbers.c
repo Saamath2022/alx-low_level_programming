49055ed870bf1b3e9857d73ef704bf4a4f25babd
Saamath2022 <saaaondow@gmail.com>
2023-06-24 03:42:55 -0700

    task 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,8 +8,11 @@
 
 void print_numbers(void)
 {
-	char numbers[] = "012345678\n";
+	char c;
 
-	_putchar(number[0]);
-	_putchar(numbers[9]);
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
