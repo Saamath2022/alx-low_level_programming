f8216540d0377ed9ebaedf72a26fb80067946f79
Saamath2022 <saaaondow@gmail.com>
2023-06-25 06:58:24 -0700

    task 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * print_line - function that draws a straight lin
- * @n: Where n is the number of the character
- * @a: the line should end with a\n
- * Return: 0 Always
+ * print_line - Draws a straight line according to parameter
+ * @n: the number of line to draw
+ * Return: empty
  */
 
 void print_line(int n)
 {
+	int x;
+
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
 	else
 	{
-	while (i = 0; i < n; i++)
+	for (x = 0; x < n; x++)
 	{
-	_putchar('_');
+	_putchar(95);
 	}
 	_putchar('\n');
 	}
