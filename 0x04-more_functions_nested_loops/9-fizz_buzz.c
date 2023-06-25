0e22ceb1edaf149501f38823e7365b5fafcc72b4
Saamath2022 <saaaondow@gmail.com>
2023-06-25 10:58:56 -0700

    task 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints "Fizz-Buzz test" the number 1 to 100.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+	if ((x % 1 == 0) && (x % 5 == 0))
+	{
+	printf("FizzBuzz ");
+	}
+	else if (x % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else if (x % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+
+	return (0);
+}
