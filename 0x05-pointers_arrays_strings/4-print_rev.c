cfcd1415923a076c29a5662dbf4ece19b6ddadd6
Saamath2022 <saaaondow@gmail.com>
2023-06-26 13:31:17 -0700

    task 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @a: the string to print
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+	_putchar(s[c]);
+	}
+	_putchar('\n');
+}
+
