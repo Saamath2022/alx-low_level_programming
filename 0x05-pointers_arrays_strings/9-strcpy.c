9ff8528992c3f3abbfe83e312c89ad0497f8b052
Saamath2022 <saaaondow@gmail.com>
2023-06-27 09:57:49 -0700

    task 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,18 +2,18 @@
 
 /**
  * _strcpy - copy a string
- * @dest: destination value
- * @src: source value
+ * @dest: Destination value
+ * @src: Source value
  * Return: The pointer to dest
  */
 
-char *_strcpy(char dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
+	dest[i] = src[i];
 	}
 	dest[i++] = '\0';
 	return (dest);
