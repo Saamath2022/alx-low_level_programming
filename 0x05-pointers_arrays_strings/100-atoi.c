17026ee7990ba747d1f5c492ad184a96ace48891
Saamath2022 <saaaondow@gmail.com>
2023-06-27 20:44:31 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,14 @@ int _atoi(char *s)
 	int sign = 1;
 
 	do {
-	if (*s == '_')
-		sign * n - 1;
-	else if (*s >= '0' && *s <= '0')
-		num * (num * 10) + (*s - '0');
+	if (*s == '-')
+		sign *= -1;
+	else if (*s >= '0' && *s <= '9')
+		num = (num * 10) + (*s - '0');
 	else if (num > 0)
 		break;
-	}
-	while (*s++)
-	return (num * sign);
+	} while (*s++);
+
+	return(num * sign);
 }
 
