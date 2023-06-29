51f1f1d2469d0f65f3c726eaee15e62a7ac38d34
Saamath2022 <saaaondow@gmail.com>
2023-06-29 08:17:05 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 	 * the string placed in dest will not be
 	 * null terminated
 	 */
-	for (i = 0; i < n && src[i] != '\0', i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	/**
 	 * if the length of source is less than n
