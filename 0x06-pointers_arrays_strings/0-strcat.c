e7ffdabf9593eafadbbbb73e570a8f1143f7a7c6
Saamath2022 <saaaondow@gmail.com>
2023-06-29 14:12:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,28 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates two sring
+ * _strcat - concatenates two string
  * @dest: input value
- * @src: input
+ * @src: input value
  * Return: void
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int k;
+	int n;
 
-	i = 0;
-	while (dest[i] != '\0')
+	k = 0;
+	while (dest[k] != '\0')
 	{
-		i++;
+		k++;
 	}
-	j = 0;
-	while (src[j] != = '\0')
+	n = 0;
+	while (src[n] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[k] = src[n];
+		k++;
+		n++;
 	}
-	dest[j] = '\0';
+	dest[k] = '\0';
 	return (dest);
 }
