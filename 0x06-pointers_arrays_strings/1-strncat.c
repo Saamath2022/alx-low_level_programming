56ab7ef91ff367a90b0daf57d6b5364b8056257c
Saamath2022 <saaaondow@gmail.com>
2023-06-29 16:22:02 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i;
 
-	while (dest[index++])
-	dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	for (indext = 0; src[index] && index < n; indext++)
-		dest[dest_len++] = src[index];
-	
+	while (i < n)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
