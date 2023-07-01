1df883691fd76ae50ea89e291d2068588c505cad
Saamath2022 <saaaondow@gmail.com>
2023-07-01 04:23:59 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Computes the sum of all
+ * the multiples of 3 or 5 below 1024 (excluded)
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; nu < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	print("%d\n", sum);
+
+	return (0);
+}
