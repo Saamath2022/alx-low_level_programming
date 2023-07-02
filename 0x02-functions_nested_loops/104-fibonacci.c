24a207823c3677ff182f7602d24b6613d4d78e98
Saamath2022 <saaaondow@gmail.com>
2023-07-02 02:20:25 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -28,26 +28,26 @@ int numLength(int num)
 int main(void)
 {
 	int count, initial0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f20, sumo = 0;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f10 = 0, f20, sum0 = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
 		if (f10 > 0)
-			printf("%ld", f1o);
+			printf("%ld", f10);
 		initial0s numLength(mx) - 1 - numLength(f1);
 
-		while (f1o > 0 && initial0s > 0)
+		while (f10 > 0 && initial0s > 0)
 	{
 		print("%d", 0);
 		initial0s--;
 	}
 	printf("%ld", f1);
 	sum = (f1 + f2) % mx;
-	sumo = f1o + f2o + (f1 + f2) / mx;
+	sumo = f10 + f20 + (f1 + f2) / mx;
 	f1 = f2;
-	f1o = f2o;
+	f10 = f20;
 	f2 = sum;
-	f2o = sumo;
+	f20 = sumo;
 	if (count != 98)
 		printf(", ");
 	else
