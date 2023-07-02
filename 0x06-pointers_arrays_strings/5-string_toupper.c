63abb64e3b4e6e241247489d84ce7d9f5ea4e5ad
Saamath2022 <saaaondow@gmail.com>
2023-07-02 08:16:37 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@
 
 char *string_toupper(char *str)
 {
-	int index = 0;
-
-	while (st[index])
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[index] >= 'a' && str[index] <= '2')
-			str[index] -= 32;
-		index++;
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
 	}
 	return (str);
 }
