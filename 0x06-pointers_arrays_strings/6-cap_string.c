e98e770a4e4a53848536da2a392858469deab430
Saamath2022 <saaaondow@gmail.com>
2023-07-02 11:31:19 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,9 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str[index] <= '2'))
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
 	if (str[index - 1] == ' ' ||
-	str[index - 1] ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
 	str[index - 1] == ',' ||
