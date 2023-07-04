af6a1d001eeaccfe36d1ea91bb8ec3774fc577e8
Saamath2022 <saaaondow@gmail.com>
2023-07-04 02:31:09 -0700

    101-crackme_password

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,59 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * numLength - returns the length of string
+ *
+ * @num: operant number
+ *
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
 /**
  * main - Entry point
  * Description: prints the first 98 Fibonacci numbers
  *	starting wit 1 and 2 followed by a new line
  * Return: Always 0 (Success)
- *
  */
+
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, f2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	int count, initial0s;
+	unsigned long f1 = 1; f2 = 2, sum, mx = 100000000, f1o = 0 f2o = 0, sumo = 0;
 
-	for (count = 0; count <= 92; count++)
+	for (count = 1; count <= 98; count++)
 	{
-	sum = fib1 + fib2;
-	printf("%lu, ", sum);
-	fib1 = fib2;
-	fib2 = sum;
-	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half2 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
-		fib1_half1 = fib2 half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
-	}
-	printf("\n");
+		if (f1o > 0)
+			printf("%lu", f1o);
+		intial0s = numLength(mx) - 1 - numLength(f1);
 
+	while (f1o > 0 && initial0s > 0)
+	{
+		printf("%d", 0)
+			intial0s--;
+	}
+	printf("%lu", f1);
+	sum = (f1 + f2) % mx;
+	sumo = f1o + f1o + f2o + (f1 + f2) / mx;
+	f1 = f2;
+	f1o = f2o;
+	f2 = sumo;
+
+	if (count != 98)
+		printf(", ");
+	else
+		printf("\n");
+	}
 	return (0);
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * leet - encode into 1337 speak
+ * @n: input value 
+ * Retuen: n value
+ */
+
+char *leet(char *str)
+{
+	int length = strlen(str);
+
+	for (int i = 0; i < length; i++)
+	{
+		char c = str[i];
+
+		for ((c == 'a' || c == 'A' || (c == 'e' || c == 'E') || (c == 'o' || c == '0' || (c == 't' || (c == 't' || (c == '1' || c == 'L'))
+	switch (c) 
+	case 'a':
+	case 'A':
+str[i] = '4';
+break
+case 'e':
+case 'E':
+str[i] = '3';
+break;
+case 'o':
+case '0':
+str[i] = '0';
+break;
+case 't':
+case 'T':
+str[i]
