66e0531d7e1da830b1f310366fd6939e4ec8b02f
Saamath2022 <saaaondow@gmail.com>
2023-07-06 03:13:14 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * _put _recursion - prints a string, following by a new line.
- * @s: The string
+ * _put _recursion - prints string in reverse.
+ * @s: Paramater of string
  * Return: void
  */
-void _put_recursion (char *s)
+void _put_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s > '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putcha char(*s);
-	s++;
-	_puts_recursion(s);
 }
