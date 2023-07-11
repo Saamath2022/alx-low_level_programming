b409492ba67f7696e0638c7161c034c5440ef7a3
Saamath2022 <saaaondow@gmail.com>
2023-07-11 02:53:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,35 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 
 /**
- * main - Entry point
- * @argc: This is the number
- * @argv: This is the array
- *
- * Return: Always 0.
+ *main-Print sum of 2 numbers.
+ *@argc:Number of commadline arguments.
+ *argv:pointer to an array of command line arguments
+ *Return:0-success, non-zero-fail.
  */
 int main(int argc, char *argv[])
 {
-	long result = 1;
+	int sum;
 
-	for (y = 1; y < argc; y++)
+	if (argc == 3)
 	{
-		long num;
-		char *p;
-
-		errno = 0;
-		num = strtol(argv[y], &p, 10);
-
-		if (*p != '\0' || errno != 0)
-		{
-			fprintf(stderr, "Conversion of argv[%] failed!\n", y);
-			exit(EXIT_FAILURE);
-		}
-		result *= num;
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
 	}
-	printf("%ld\n", result);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-	return (EXIT_SUCCESS);
+	return (0);
 }
