0f1fdcc8c13607c003b6e61fd412b567b03b64ad
Saamath2022 <saaaondow@gmail.com>
2023-10-15 07:59:11 -0700

    all the changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,34 +1,20 @@
-#include <stdio.h>
-
-void _puts_recursion(char *s);
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
-}
-
-/**
- * _puts_recursion - Print a string followed by a new line
- * @s: the string to print
- *
- * Return: Void
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: String pointer to be printed
+ * Return: Nothing
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
-		return;
+		_putchar('\n');
 	}
-	putchar(*s);
-	s++;
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
-
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -2,12 +2,11 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @c: This is my _putchar.c
+ * Return: on success 1.
  */
+
 int _putchar(char c)
 {
-	return write(STDOUT_FILENO, &c, 1);
+	return (write(TDOUT_FILENO, &c, 1));
 }
