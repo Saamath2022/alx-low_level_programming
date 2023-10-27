f24de97310fa9f075ff1b349ef4ddb441966b52d
saamath2022 <saaaondow@gmail.com>
2023-10-27 10:07:26 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,14 +13,15 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
+
 	if (*s2 == *s1 || *s2 == '*')
 	{
-	if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1))
+		if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1))
 		{
 			return (1);
 		}
 	}
-	if (*s2 == '?')
+	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
