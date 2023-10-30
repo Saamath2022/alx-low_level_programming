a46d6bc3b763e11e030b2a55cbb8e82484664279
saamath2022 <saaaondow@gmail.com>
2023-10-30 11:00:02 -0700

    tast 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all arguments that recieveds.
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always nothing
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i = 0;
+
+	(void)argv;
+
+	while (i < argc)
+	{
+		i++;
+	}
+	return (0);
+}
