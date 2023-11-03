524858a78e09a298c0f398e2ad300fd6914416ea
saamath2022 <saaaondow@gmail.com>
2023-11-02 19:03:50 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(b);
+	int *ptr = malloc(b);
 
-	if (m == 0)
-		exit(90);
+	if (ptr == 0)
+		exit(98);
 
-	return (m);
+	return (ptr);
 }
