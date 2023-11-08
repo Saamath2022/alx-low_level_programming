c4c1213f30fc94ca8ca3061784d208d3ce89f6b7
saamath2022 <saaaondow@gmail.com>
2023-11-07 18:29:01 -0800

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_dog - structure of a dog
+ * @d: ...
+ * Return: Return 1 if negative or 0 is positive.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return (d);
+	}
+}
