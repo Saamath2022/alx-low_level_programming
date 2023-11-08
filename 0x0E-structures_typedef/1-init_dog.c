0272c8ea88de7e6abd40a6802f4d17b9ef611a04
saamath2022 <saaaondow@gmail.com>
2023-11-07 17:59:09 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <dog.h>
+
+/**
+ * init_dog - Initialize a dog structure
+ * @d: pointer to struct dog
+ * @name: name to be initialized
+ * @age: age to inititialized
+ * @owner: the do's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = strdup(name);
+		d->age = age;
+		d->owner = strdup(owner);
+
+	}
+}
+
