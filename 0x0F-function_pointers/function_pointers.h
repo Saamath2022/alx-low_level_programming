e188c19468758b8a3417a0e477f942a118e82a1d
saamath2022 <saaaondow@gmail.com>
2023-11-09 04:33:22 -0800

    task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_h
+#define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
 #endif
+
