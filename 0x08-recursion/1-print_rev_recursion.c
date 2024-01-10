9e365e4854d6e2ba1492ed53bede31f20b2dd618
Saamath2023 <saaaondow@gmail.com>
2024-01-10 08:48:09 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * _print_rev_recursion - Prints a string in revere using recursion
  * @s: The input string to be reversed
  * Description: This function takes a string as input recursively
