238f1a0b44f3e5e05f5df1f2e64c5a7c46a7a50c
Saamath2023 <saaaondow@gmail.com>
2024-01-12 17:56:43 -0800

    main.h

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute
+ * value of an integer.
+ *@n: take integer type input for function.
+ * Return: Always bsolute value of number or zero.
+ */
+i
+int _abs(int n)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
