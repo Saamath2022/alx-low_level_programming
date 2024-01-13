f4b0102a7e0adc861d6d497890c6b6024ff667a0
Saamath2023 <saaaondow@gmail.com>
2024-01-12 20:29:40 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * function - A program that print its name
+ * @arg: arg count
+ * @argv: argument vector
+ * Return: Always zero
+ */
+
+int function(int arg, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
