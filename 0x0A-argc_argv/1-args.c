5103fd7057469992c589c83bcdc83323ece94a4f
Saamath2023 <saaaondow@gmail.com>
2024-01-13 18:26:18 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - count the number of argms that are passed
+ * @argc:	An integer parameters
+ * @argv:	An array parameters that are passed
+ * Return: Always 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("Number of Arguments: %d\n", argc);
+	return (0);
+}
