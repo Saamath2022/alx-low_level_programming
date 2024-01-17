f7c338088dd4e4092929bd401998f7197b5fb8dc
Saamath2023 <saaaondow@gmail.com>
2024-01-16 19:33:12 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,39 +10,42 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	int y = 0, x = 0, k = 0, u = 0;
+	char *s;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t len_s1 = 0;
-	size_t len_s2 = 0;
-
-	while (s1[len_s1] != '\0')
+	while (s1[y] != '\0')
 	{
-		lent_s1++;
+		y++;
 	}
-	while (s2[len_s2] != '\0')
+	while (s2[x] != '\0')
 	{
-		len_s2++;
+		x++;
 	}
 
-	char *result = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char))
+	k = y + x;
+	s = malloc((sizeof(char) * (k + 1)));
 
-	if (result == NULL)
-	{
+	if (s == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < len_s1; ++)
+	x = 0;
+	while (k > u)
 	{
-		result[i];
+		if (k <= y)
+			s[k] = s1[x];
+		if (k >= y)
+		{
+			s[k] = s2[y];
+			y++;
+		}
+		k++;
 	}
-
-	for (i = 0; i < lens_s2; i++)
-	{
-		result[len_s1 + i] = s2[i];
-	}
-	return (result);
+	s[k] = '\0';
+	return (s);
 }
 
