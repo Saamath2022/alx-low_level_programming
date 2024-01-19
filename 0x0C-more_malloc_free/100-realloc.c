7a18770a757e860919d4533c0c1d061ca19ff2be
Saamath2023 <saaaondow@gmail.com>
2024-01-19 07:45:56 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,31 +16,31 @@ void* _realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p;
 	
 	if (new_size == old_size)
-		return ptr;
+		return (ptr);
 
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
 		if (p == NULL)
-			return NULL;
-		return p;
+			return (NULL);
+		return (p);
 	}
 	else
 	{
 		if (new_size == 0)
 		{
 			free(ptr);
-			return NULL;
+			return (NULL);
 		}
 	}
        p = malloc(new_size);
        if (p == NULL)
-	       return NULL;
+	       return (NULL);
        
        for (i = 0; i < old_size && i < new_size; i++)
        {
 	       p[i] = ((char *)ptr)[i];
        }
        free(ptr);
-       return p;
+       return (p);
 }
