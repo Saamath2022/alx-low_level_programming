d14182f7ad4873efb391991e151d37cb38642a23
Saamath2023 <saaaondow@gmail.com>
2024-01-19 07:41:43 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,26 +10,37 @@
  * Return: Always 0 othwise 1.
  */
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void* _realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
 	unsigned int i;
-
+	char *p;
+	
 	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+		return ptr;
+
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
 		if (p == NULL)
-			return (NULL);
-		for (i = 0; i < old_size && i < new_size; i++)
-			*((char *) p + i) = *((char *)ptr + i);
-		free(ptr);
+			return NULL;
+		return p;
 	}
-	return (p);
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return NULL;
+		}
+	}
+       p = malloc(new_size);
+       if (p == NULL)
+	       return NULL;
+       
+       for (i = 0; i < old_size && i < new_size; i++)
+       {
+	       p[i] = ((char *)ptr)[i];
+       }
+       free(ptr);
+       return p;
 }
