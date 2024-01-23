d53c41b8eb83c2ada0aa3ef9d22affdc56f80fb5
Saamath2023 <saaaondow@gmail.com>
2024-01-22 17:18:04 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file
+ * main - Prints the name of file followd by a new line
  * Return: Always 0.
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
-
