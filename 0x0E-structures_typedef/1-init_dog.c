9d4c8f4ad48c2e22e3b372f184d7f709df6e0f61
Saamath2023 <saaaondow@gmail.com>
2024-01-23 17:24:27 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,34 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * init_dog - Represents information about a dog
- * @name: name of the  dog
- * @age: age of the dog
- * @owner: Owner of the dog.
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-#endif /* DOG_H */
-
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
- * init_dog - Intializes a variabe of type struct dog.
+ * init_dog - Intializes a variablie of type struct dog.
  * @d: pointer to the struct dog
  * @name: nae of the dog
  * @age: Age of the og
- * owner: Onwer of the dog.
- * Description: this function initilizes the members of struct dog
- * with the provide values.
+ * @owner: Onwer of the dog.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -38,22 +16,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->owner = owner;
 	}
-}
-/**
- * main - entry point of the programme
- * Return: always 0 (success)
- */
-int main(void)
-{
-	struct dog myDog;
-
-	init_dog(&myDog, "Doggy", 3.5, "Bush Man");
-
-	printf("Name: %s\n", myDog.name);
-	printf("Age: %.2f\n", myDog.age);
-	printf("Owner: %s\n", myDog.owner);
-
-
-	return (0);
 }
 
