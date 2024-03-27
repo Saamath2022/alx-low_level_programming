9cb3feaf2965ea3ff732e5c909dc72065731e0cb
Saamath2023 <saaaondow@gmail.com>
2024-03-26 19:37:15 -0700

    main.h libdynamic.so

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: Most number of byte from &src
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[1] = '\0';
+
+	return (dest);
+}
